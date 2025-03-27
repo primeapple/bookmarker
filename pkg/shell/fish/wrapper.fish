e3eb3a0817ac9d5c0137c053d0f5e671bfbaa0c7
primeapple <toni.mueller.web@mailbox.org>
2025-03-27 20:53:47 +0100

    Fixed fish completion for get

--- pkg/shell/fish/wrapper.fish
+++ pkg/shell/fish/wrapper.fish
@@ -22,8 +22,8 @@ end
 set -l options -a --add -g --get -l --list
 
 complete -c bm --no-file
-complete -c bm --condition "not __fish_seen_subcommand_from $options" --require-parameter --arguments "(bm --list | awk '{ print \$2 }')" --description "Change directory to bookmark path"
+complete -c bm --condition "not __fish_seen_subcommand_from $options" --require-parameter --arguments "(bm --list | awk '{ print \$1 }')" --description "Change directory to bookmark path"
 complete -c bm --old-option a --long-option add --description "Add named bookmark to current directory"
-complete -c bm --old-option g --long-option get --require-parameter --arguments "(bm --list | awk '{ print \$2 }')" --description "Print path to bookmark"
+complete -c bm --old-option g --long-option get --require-parameter --arguments "(bm --list | awk '{ print \$1 }')" --description "Print path to bookmark"
 complete -c bm --old-option h --long-option help --description "Displays help for the 'bm' command"
 complete -c bm --old-option l --long-option list --description "List all available bookmarks with their index and path"
